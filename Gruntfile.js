305b410beead49a134976e0e787a26ed115778fa
AlexBilozir <alexbilozir@gmail.com>
2015-01-08 20:42:14 +0200

    First pages commit

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: ['js/libs/*.js', 'js/global.js'], // input
+        dest: 'js/build/global.min.js' // output
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
